2ce1b10e64ff29ffa9343f63e8a6341711f0accf
João Augusto <joao.augusto@foodzilla.com.br>
2021-09-17 00:55:43 -0300

    feat: adding fav icon

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import Project from "@components/Project";
 import Title from "@components/Title";
 import ContainerHello from "components/ContainerHello";
@@ -5,6 +7,9 @@ import ContainerHello from "components/ContainerHello";
 export default function Home() {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+      </Head>
       <ContainerHello /> <Title title="Projetos" /> <Project />
     </>
   );
